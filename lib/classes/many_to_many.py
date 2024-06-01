3da1cb595c07b40e3e19645a2ee845257d8f319e
Tracy Karanja <chirutracy@gmail.com>
2024-06-01 18:24:04 +0300

    refactor: Add methods to retrieve articles, magazines, and authors in ManyToMany class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,10 @@
 class Article:
+    all_articles = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all_articles.append(self)
         
     @property
     def title(self):
@@ -14,7 +16,7 @@ class Article:
         if hasattr(self, '_title'):
             raise AttributeError("Cannot change the title after it has been set")
         if isinstance(title, str) and 5 <= len(title) <= 50:
-            return self._title
+            self._title = title
         else:
             raise ValueError("Title must be a non-empty string.")
         
@@ -59,24 +61,31 @@ class Author:
         if isinstance(name, str) and len(name) > 0:
             self._name = name
         else:
-            raise ValueError("Name must be an non-empty string.")
-
+            raise ValueError("Name must be a non-empty string.")
+    # Returns a list of all the articles the author has written
     def articles(self):
-        pass
-
+        return [article for article in Article.all_articles if article.author == self]
+    
+    # Returns a unique list of magazines for which the author has contributed to
     def magazines(self):
-        pass
+        return [article.magazine for article in self.articles()]
 
+    # Receives a Magazine instance, and a title as arguments | Creates and returns a new Article instance and associates it with that author, the magazine provided
     def add_article(self, magazine, title):
-        pass
+        Magazine(title)
+        Article(title, self, magazine)
 
+    # Returns a unique list of strings with the categories of the magazines the author has contributed to
     def topic_areas(self):
-        pass
+        return [category for category in Magazine.category if category == self]
 
 class Magazine:
+    all = []
+    
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
         
     @property
     def name(self):
@@ -86,7 +95,7 @@ class Magazine:
     def name(self, name):
         """Name must be of type str, must be between 2 and 16 characters inclusive, should be able to change after the magazine is instantiated."""
         if isinstance(name, str) and 2 <= len(name) <= 16:
-            return self._name
+            self._name = name
         else:
             raise ValueError("Name must be between 2 and 16 characters long.")
         
@@ -102,14 +111,20 @@ class Magazine:
         else:
             raise ValueError("Category must be an non-empty string.")
 
+    # Returns a list of all the articles the magazine has published
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.magazine == self]
 
+    # Returns a list of authors who have contributed to that magazine
     def contributors(self):
-        pass
+        return [article.author for article in self.articles()]
 
+    # Returns a list of the titles strings of all articles written for that magazine
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        # should return a list of authors who have written more than 2 articles for the magazine
+        from collections import Counter
+        author_count = Counter(article.author for article in self.articles())
+        return [author for author,count in author_count.items() if count > 2]
